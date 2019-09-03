32ced734c760a3257d397c6c0307b0c82b425b01
haavisto <54839630+haavisto@users.noreply.github.com>
2019-09-03 12:15:40 +0300

    Update Team.cs

--- KanaliigaTeamTracker/Team.cs
+++ KanaliigaTeamTracker/Team.cs
@@ -8,13 +8,12 @@ namespace KanaliigaTeamTracker
 {
     class Team
     {
-        //Template for rocket league & max 3 players
+        //Template for rocket league & max 4 players
         public string TeamName { get; set; }
         public string Rank1 { get; set; }
         public string Rank2 { get; set; }
         public string Rank3 { get; set; }
         public string Rank4 { get; set; }
-        public string AvgRank { get; set; }
         public string Player1 { get; set; }
         public string Player2 { get; set; }
         public string Player3 { get; set; }
